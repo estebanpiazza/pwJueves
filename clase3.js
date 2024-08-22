3fd3c60cb0f83b66d5cf8d27c0cd2769f9e08184
Esteban Piazza <estebanpiazza51@gmail.com>
2024-08-22 19:20:25 -0300

    clase 2 creada

---
+++ clase3.js
@@ -0,0 +1,18 @@
+//Funciones
+
+// Expresadas
+//declarando
+console.log(nombreDeLaFuncion(2,3))
+function nombreDeLaFuncion(a,b) {
+    return a+b
+}
+//invocando
+console.log(nombreDeLaFuncion(1,2))
+// let y var
+let nombre = "Esteban"
+function saludar(){
+    let nombre = "Esteban"
+    return "Hola" + nombre
+}
+
+console.log(saludar())
